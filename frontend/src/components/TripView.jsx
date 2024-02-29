9d2665c45ccb6b72f722266bbd7e36a293f749cd
Ahamed Kabeer <ahamedkabeer@zealeye.com>
2024-02-29 10:47:11 +0530

    Show movement in map

--- frontend/src/components/TripView.jsx
+++ frontend/src/components/TripView.jsx
@@ -7,17 +7,37 @@ const TripView = ({ tripStartPos, tripEndPos, positions }) => {
 
   const [map, setMap] = useState(null);
   const [path, setPath] = useState([]);
+  const [position, setPosition] = useState({ lat: tripStartPos.lat, lng: tripEndPos.long });
+
+  let index = 0;
 
   const onLoad = useCallback((map) => setMap(map), []);
+
   useEffect(() => {
     if (map) {
       const bounds = new window.google.maps.LatLngBounds();
-      console.log(positions);
       positions.map((p) => {
         setPath((prev) => ([...prev, { lat: p.latitude, lng: p.longitude }]));
         bounds.extend({ lat: p.latitude, lng: p.longitude });
       });
       map.fitBounds(bounds);
+      console.log(path);
+      const intervalId = setInterval(() => {
+        console.log("OK", index);
+        const newLat = positions[index].latitude;
+        const newLng = positions[index].longitude;
+
+        console.log({ lat: newLat, lng: newLng, tripEndPos });
+        setPosition({ lat: newLat, lng: newLng });
+
+        if (newLat === tripEndPos.lat && newLng === tripEndPos.long) {
+          console.log("Hmm");
+          clearInterval(intervalId);
+        }
+        index++;
+      }, 750);
+
+      return () => clearInterval(intervalId); // Cleanup on component unmount
     }
   }, [map, tripStartPos, tripEndPos, positions]);
 
@@ -34,11 +54,12 @@ const TripView = ({ tripStartPos, tripEndPos, positions }) => {
       ) : (
         <GoogleMap
           mapContainerClassName="map-container"
-
           onLoad={onLoad}
+          mapTypeId={window.google.maps.MapTypeId.ROADMAP}
         >
-          <MarkerF position={{ lat: tripStartPos.lat, lng: tripStartPos.long }} />
-          <MarkerF position={{ lat: tripEndPos.lat, lng: tripEndPos.long }} />
+          <MarkerF position={{ lat: tripStartPos.lat, lng: tripStartPos.long }} icon={{ url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png" }} />
+          <MarkerF position={{ lat: tripEndPos.lat, lng: tripEndPos.long }} icon={{ url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png" }} />
+          <MarkerF position={position} />
           <PolylineF
             path={path}
             options={{
